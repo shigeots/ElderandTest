5df4d683bdd4ee58e4c90fc4780d342f6e3f9e5a
shigeots <shigeots@gmail.com>
2022-04-02 18:59:38 -0400

    Update BossColliderController.cs

--- Assets/Scripts/Boss/Gargoyle/BossColliderController.cs
+++ Assets/Scripts/Boss/Gargoyle/BossColliderController.cs
@@ -9,6 +9,7 @@ public class BossColliderController : MonoBehaviour {
     private BossCoreController _bossCoreController;
 
     private const string TAG_GROUND = "Ground";
+    private const string TAG_DAMAGE_ENEMY = "DamageTheEnemy";
 
     #endregion
 
@@ -25,25 +26,29 @@ public class BossColliderController : MonoBehaviour {
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.CompareTag(TAG_GROUND)) {
+        if(other.gameObject.CompareTag(TAG_GROUND)) {
             isGrounded = true;
+        }
+
+        if(other.gameObject.CompareTag(TAG_DAMAGE_ENEMY)) {
+            Debug.Log("Recibir daño");
         }
     }
 
     private void OnTriggerExit2D(Collider2D other) {
-        if (other.gameObject.CompareTag(TAG_GROUND)) {
+        if(other.gameObject.CompareTag(TAG_GROUND)) {
             isGrounded = false;
         }
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
-        if (other.gameObject.CompareTag(TAG_GROUND)) {
+        if(other.gameObject.CompareTag(TAG_GROUND)) {
             isGrounded = true;
         }
     }
 
     private void OnCollisionExit2D(Collision2D other) {
-        if (other.gameObject.CompareTag(TAG_GROUND)) {
+        if(other.gameObject.CompareTag(TAG_GROUND)) {
             isGrounded = false;
         }
     }
