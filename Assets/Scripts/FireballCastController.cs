c3b4b6a6885e5593f92453791e271ffe34840944
shigeots <shigeots@gmail.com>
2022-04-03 04:26:21 -0400

    Created script FireballCastController

---
+++ Assets/Scripts/FireballCastController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballCastController : MonoBehaviour {
+
+    #region Private methods
+
+    private void DestroyThisGameObject() {
+        Destroy(gameObject);
+    }
+
+    #endregion
+}
